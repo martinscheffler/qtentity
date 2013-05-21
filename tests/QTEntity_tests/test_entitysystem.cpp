1e2941b4c9f5ed9896b91fcc89eee0a0c63f59e7
wooyay@web.de <wooyay@web.de>
2013-05-21 05:47:56 +0200

    some shuffling around to get test system up

---
+++ tests/QTEntity_tests/test_entitysystem.cpp
@@ -0,0 +1,27 @@
+#include <QtTest/QtTest>
+#include <QtCore/QObject>
+
+#include <QTEntity/EntitySystem>
+
+class EntitySystemTest: public QObject
+{
+    Q_OBJECT
+private slots:
+    void initTestCase()
+    {
+    }
+
+    void cleanupTestCase()
+    {
+    }
+
+    void myFirstTest()
+    { QVERIFY(1 == 1); }
+
+    void mySecondTest()
+    { QVERIFY(1 != 2); }
+
+};
+
+QTEST_GUILESS_MAIN(EntitySystemTest)
+#include "test_entitysystem.moc"
