b373e68a127b0bfdb16ac4e69127cbcf0b12c907
wooyay <wooyay@web.de>
2013-07-05 14:40:46 +0200

    adding mutex protection to MetaObjectRegistry.cpp

--- source/QtEntity/MetaObjectRegistry.cpp
+++ source/QtEntity/MetaObjectRegistry.cpp
@@ -12,11 +12,13 @@ namespace QtEntity
 	
     void registerMetaObject(const QMetaObject& mo)
     {
+        QMutexLocker m(&s_metaObjectRegistryMutex);
         s_metaObjects[QString(mo.className())] = &mo;
     }
 
     const QMetaObject* metaObjectByClassName(const QString& classname)
     {
+        QMutexLocker m(&s_metaObjectRegistryMutex);
         auto i = s_metaObjects.find(classname);
         return (i == s_metaObjects.end()) ? nullptr : i.value();
     }
