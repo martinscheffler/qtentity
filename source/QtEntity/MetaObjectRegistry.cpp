37db11df6c72280093c1dca98f5d70ea8301485e
martin <martin@linux.chaos>
2013-08-30 10:00:59 +0200

    moved componentregistry to entitysystem header and cpp

--- source/QtEntity/MetaObjectRegistry.cpp
+++ source/QtEntity/MetaObjectRegistry.cpp
@@ -26,32 +26,4 @@ namespace QtEntity
         return (i == s_metaObjects.end()) ? nullptr : i->second;
     }
 
-
-    std::unordered_map<std::string, ClassTypeId> s_classTypeIds;
-    std::unordered_map<ClassTypeId, std::string> s_classNames;
-    
-    ClassTypeId ComponentRegistry::registerComponent(const QString& classname)
-    {
-        Q_ASSERT(s_classTypeIds.find(classname.toStdString()) == s_classTypeIds.end());
-        ClassTypeId s = (ClassTypeId) s_classTypeIds.size();        
-        s_classTypeIds[classname.toStdString()] = s;
-        s_classNames[s] = classname.toStdString();
-        return s;
-    }
-
-    ClassTypeId ComponentRegistry::classTypeId(const QString& classname)
-    {
-        auto i = s_classTypeIds.find(classname.toStdString());
-        if(i == s_classTypeIds.end()) return -1;
-        return i->second;
-    }
-
-
-    QString ComponentRegistry::className(ClassTypeId typeId)
-    {
-        auto i = s_classNames.find(typeId);
-        if(i == s_classNames.end()) return "<Class not registered>";
-        return QString::fromStdString(i->second);
-    }
-
 }
